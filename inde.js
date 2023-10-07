8f7abada9cdff9a2f3221842b12543b5c3274417
BASURAMAKOTIREDDY <basu.ramakotireddy13@gmail.com>
2023-10-07 15:32:56 +0530

    first commit

---
+++ inde.js
@@ -0,0 +1,17 @@
+// const arr=[1,2,3,4,5];
+const arr=[
+    { name:"umakath", age: 25},
+    { name:"sriram", age:24},
+    { name:"ram",age:28}
+];
+
+
+
+
+
+const test =arr.map((each)=>(
+each.name+" "+each.age
+)); 
+
+console.log(arr);
+console.log(test);
